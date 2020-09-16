2bf397ac6fb761d3790fab0bf7375b5cd7e0adc0
mikeclueby4 <mikeclueby4@gmail.com>
2020-09-16 04:24:06 +0200

    Update serve.sh
    serve.sh now does auto-update EVEN in a VM working on a shared folder

--- serve.sh
+++ serve.sh
@@ -1,1 +1,2 @@
-bundle exec jekyll serve --watch
+# use force_polling to work when we're running in a VM on a shared folder from the host -- file system notifications don't seem to happen like they should
+bundle exec jekyll serve --watch --force_polling
